6c8220974d3bb63ded4d4e916e0115c5c217e909
Sherief Elsowiny <elsowiny@yahoo.com>
2021-07-31 16:07:25 -0400

    refactor - moved data to another python file

---
+++ localdata.py
@@ -0,0 +1,89 @@
+people = {
+    1:{
+        "name":"John",
+        "age":25,
+        "address":"New York"
+
+    },
+    2:{
+        "name":"Jack",
+        "age":24,
+        "address":"New York"
+    },
+    3:{
+        "name":"Joe",
+        "age":26,
+        "address":"New York"
+    },
+    4:{
+        "name":"Jack",
+        "age":24,
+        "address":"New Mexico"
+    },
+    5:{
+        "name":"Jill",
+        "age":25,
+        "address":"New York"
+    },
+    6:{
+        "name":"Jane",
+        "age":58,
+        "address":"Britain"
+    },
+    7:{
+        "name":"Jake",
+        "age":2,
+        "address":"USA"
+    },
+    8:{
+        "name":"Jake",
+        "age":13,
+        "address":"China"
+    },
+    9:{
+        "name":"Joanna",
+        "age":4,
+        "address":"USA"
+    },
+    10:{
+        "name":"Jilly Jane",
+        "age":46,
+        "address":"Lithuana",
+        "job":"Teacher" #?
+    },
+    11:{
+        "name":"Sherief",
+        "age":12,
+        "address":"Brazil"
+    },
+    13:{
+        "name":"Magnus Carlson",
+        "age":18,
+        "address":"Sweden"
+    },
+    14:{
+        "name":"Molly",
+        "age":18,
+        "address":"USA"
+    },
+    15:{
+        "name":"Molly",
+        "age":1,
+        "address":"Australia"
+    },
+    16:{
+        "name":"Molly",
+        "age":3,
+        "address":"Iceland"
+    },
+    17:{
+        "name":"Mohammed",
+        "age":18,
+        "address":"Saudia Arabia"
+    },
+    18:{
+        "name":"Steven",
+        "age":13,
+        "address":"Bolivia"
+    }
+}
